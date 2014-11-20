4a3fb7407f3e9cb26c7b26c8000b5fbeb299da41
Samuli Seppänen <samuli.seppanen@gmail.com>
2014-11-20 18:07:00 +0200

    Fixes to rdoc
    Signed-off-by: Samuli Seppänen <samuli.seppanen@gmail.com>

--- manifests/init.pp
+++ manifests/init.pp
@@ -9,7 +9,7 @@
 #
 # == Examples
 #
-# include gettext
+#   include gettext
 #
 # == Authors
 #
@@ -17,8 +17,7 @@
 #
 # == License
 #
-# BSD-lisence
-# See file LICENSE for details
+# BSD-license. See file LICENSE for details.
 #
 class gettext {
 
